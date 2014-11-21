df11bdab0f11130d8bfad616667b1c86a043e58a
jgoodyear <jgoodyear@apache.org>
2014-11-21 12:00:06 -0330

    Setting up command logic, added requirement object

--- src/main/java/com/savoirtech/karaf/commands/DependenciesList.java
+++ src/main/java/com/savoirtech/karaf/commands/DependenciesList.java
@@ -1,5 +1,5 @@
 /*
- * Analyzer 
+ * Dependencies List 
  *
  * Copyright (c) 2014, Savoir Technologies, Inc., All rights reserved.
  *
@@ -19,6 +19,18 @@
 
 package com.savoirtech.karaf.commands;
 
+import java.util.List;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.wiring.BundleCapability;
+import org.osgi.framework.wiring.BundleRequirement;
+import org.osgi.framework.wiring.BundleWire;
+import org.osgi.framework.wiring.BundleWiring;
+
+import org.apache.karaf.bundle.core.BundleService;
+
 import org.apache.karaf.shell.commands.Argument;
 import org.apache.karaf.shell.commands.Command;
 import org.apache.karaf.shell.commands.Option;
@@ -27,14 +39,53 @@ import org.apache.karaf.shell.console.OsgiCommandSupport;
 @Command(scope = "aetos", name = "dependencies-list", description = "Karaf Bundle dependencies list Command")
 public class DependenciesList extends OsgiCommandSupport {
 
-
+    @Argument(index = 0, name = "ids", description = "The list of bundle (identified by IDs or name or name/version) separated by whitespaces", required = false, multiValued = true)
+    List<String> ids;
+    
+    BundleService bundleService;
+    
     protected Object doExecute() throws Exception {
+        doExecute(true);
+        return null;
+    }
+
+    protected Object doExecute(boolean force) throws Exception {
+        List<Bundle> bundles = bundleService.selectBundles(ids, force);
+        doExecute(bundles);
+        return null;
+    }
+
+    protected void doExecute(List<Bundle> bundles) throws Exception {
         try {
-            System.out.println("Hello World");
+            System.out.println("Note: For more detail use requirements command.");
         } catch (Exception e) {
             //Ignore
         }
-        return null;
+    }
+
+    public void setBundleService(BundleService bundleSelector) {
+        this.bundleService = bundleSelector;
+    }
+
+    // Get bundle wiring
+    // Create pruned list of BundleID - URL
+    // Pretty Print table.
+
+    private class Requirement {
+
+        private String packageName;
+        private String bundleId;
+
+        public Requirement(String packageName, String bundleId) {
+            this.packageName = packageName;
+            this.bundleId = bundleId;
+        }
+
+        public String toString() {
+            String result = String.format("%4s %s", bundleId, packageName);
+            return result;
+        }
+
     }
   
 }
